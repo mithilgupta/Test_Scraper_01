511c5d312f4a40e7a5c2f46a9838cfc5d80a303c
mithilgupta <mithilgupta@gmail.com>
2024-07-25 00:46:27 -0400

    V10

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,12 +1,13 @@
 
 # GitHub Actions imported from: https://www.youtube.com/watch?v=xs9oJaaU8I8&t=265s
 import json
+from extra_utils import get_lambda_message
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! - UPDATED V8')
+        'body': json.dumps(get_lambda_message())
     }
 
 
